04207073a5d3962c63225d7dbb4d368385b0a882
Jessica Fix and torrence stratton <jcfix.pdx@gmail.com>
2016-03-23 08:31:37 -0700

    setup initial files and test gulp serve as working

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  // directives: [TaskListComponent],
+  template: `
+  <div class="container">
+    <h1>Test App!</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+  
+}
